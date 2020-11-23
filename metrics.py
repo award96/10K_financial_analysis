Serious bug fix - horizontal percent calculation
Needed to use the absolute value of the denominator in calculating the percent change of the given value. This was swapping the sign on percentages where the starting value was negative. Serious bug, now fixed.

metrics.py
@@ -5,5 +5,5 @@ def horizontal(start, end):
     val = end - start
     if (start == 0) or (start == np.nan):
         return (val, np.nan)
-    percent = val * 100 / start
+    percent = val * 100 / abs(start)
     return (val, percent)